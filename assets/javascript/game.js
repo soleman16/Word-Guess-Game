12ca17a460b0e195f86114d2cc186b00024c7341
Darren Anastasoff <soleman16@gmail.com>
2018-10-03 23:30:41 -0400

    added images and a grid system for responsive design

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var game = {
     lettersGuessed : [],
     currentPlayer : {},
     wordDisplay : "",
-    isWinner : false,
+    isGameOver : false,
     players : [
         {
             fullName: "TOM BRADY",
@@ -26,6 +26,66 @@ var game = {
             fullName: "ANDREW LUCK",
             imageLocation: "assets/images/andrew_luck.png"
         },
+        {
+            fullName: "KAREEM HUNT",
+            imageLocation: "assets/images/kareem_hunt.png"
+        },
+        {
+            fullName: "TODD GURLEY",
+            imageLocation: "assets/images/todd_gurley.png"
+        },
+        {
+            fullName: "AARON RODGERS",
+            imageLocation: "assets/images/aaron_rogers.png"
+        },
+        {
+            fullName: "ALVIN KAMARA",
+            imageLocation: "assets/images/alvin_kaxsmara.png"
+        },
+        {
+            fullName: "MELVIN GORDON",
+            imageLocation: "assets/images/melvin_gordon.png"
+        },
+        {
+            fullName: "ADAM THIELEN",
+            imageLocation: "assets/images/adam_thielen.png"
+        },
+        {
+            fullName: "SAQUON BARKLEY",
+            imageLocation: "assets/images/saquon_barkley.png"
+        },
+        {
+            fullName: "GOLDEN TATE",
+            imageLocation: "assets/images/golden_tate.png"
+        },
+        {
+            fullName: "DEANDRE HOPKINS",
+            imageLocation: "assets/images/deandre_hopkins.png"
+        },
+        {
+            fullName: "JULIO JONES",
+            imageLocation: "assets/images/julio_jones.png"
+        },
+        {
+            fullName: "ROB GRONKOWSKI",
+            imageLocation: "assets/images/rob_gronkowski.png"
+        },
+        {
+            fullName: "TRAVIS KELCE",
+            imageLocation: "assets/images/travis_kelce.png"
+        },
+        {
+            fullName: "DAVANTE ADAMS",
+            imageLocation: "assets/images/davante_adams.png"
+        },
+        {
+            fullName: "CAM NEWTON",
+            imageLocation: "assets/images/cam_newton.png"
+        },
+        {
+            fullName: "SONY MICHEL",
+            imageLocation: "assets/images/sony_michel.png"
+        },
     ],
     /**
      * @returns a randomly selected player
@@ -45,14 +105,14 @@ var game = {
         this.lettersGuessed = [];
         this.currentPlayer = this.getNewPlayer();
         this.numberOfGuessesRemaining = 10;
-        this.isWinner = false;
+        this.isGameOver = false;
         this.wordDisplay = mask(this.currentPlayer.fullName);
         this.updateHTML();
         this.displayImage("assets/images/question-mark-face.jpg");
     },
     /**
      * 
-     * @param {*} userInput 
+     * @param {String} userInput 
      * 
      * Updates the following depending if the letter exists or not:
      * If letter exists: 
@@ -77,7 +137,7 @@ var game = {
             if (this.isWordGuessed()) {
                 // play audio when the user guesses the player
                 this.playAudio("assets/audio/Touchdown!.mp4");
-                this.isWinner = true;
+                this.isGameOver = true;
                 this.wins++; 
                 this.displayImage(this.currentPlayer.imageLocation);
             }
@@ -86,11 +146,16 @@ var game = {
         // decrement the number of guesses remaing by 1
         else {
             this.numberOfGuessesRemaining--;
+            if (this.numberOfGuessesRemaining === 0)
+            {
+                this.isGameOver = true;
+                this.displayImage("assets/images/game_over.jpg")
+            }
         }
     },
     /**
      * 
-     * @param {*} userInput 
+     * @param {String} userInput 
      * 
      * Looks at the userInput and searches the players name for
      * a match. If a match is found, the word display is updated
@@ -125,16 +190,31 @@ var game = {
         return (0 === getIndicesOf("_", this.wordDisplay).length);
     },
     /**
-     * Plays a quick soundbite
+     * @param {String} audioLocation - the location
+     * of the mp3 file
+     * 
+     * Plays a soundbite at the specified location
      */
     playAudio : function(audioLocation) {
-        console.log("+++++++++++++++++++++++++++++++++++++++++++++");
         var audio = new Audio(audioLocation);
-        audio.play(50000);
+        audio.play();
     },
+    /**
+     * @param {String} imageLocation
+     * 
+     * Displays an image at the specified location 
+     */
     displayImage : function(imageLocation) {
         document.getElementById("player-image").src = imageLocation;
     },
+    /**
+     * @param {String} userInput 
+     * @returns boolean indicating if the input is 
+     * valid or not
+     * 
+     * Invalid Input - is not a letter, or the letter
+     * was already guessed. Otherwise, the input is valid
+     */
     isValidInput : function(userInput) {
         var isValid = true;
          // invalid input, display an alert message
@@ -151,12 +231,15 @@ var game = {
        
         return isValid;
     },
+    /**
+     * Updates the HTML on the page
+     */
     updateHTML : function() {
         document.getElementById("wins").innerHTML = this.wins;
         document.getElementById("current-word").innerHTML = this.wordDisplay;
         document.getElementById("guesses-remaining").innerHTML = this.numberOfGuessesRemaining;
         // removes commas from array
-        document.getElementById("letters-guessed").innerHTML = this.lettersGuessed.join(" ");
+        document.getElementById("letters-guessed").innerHTML = this.lettersGuessed.join("");
     },
     /**
      * Writes all variables to the console. Utility method for debugging
@@ -166,7 +249,7 @@ var game = {
         console.log("wins: " + this.wins);
         console.log("numberOfGuessesRemaining: " + this.numberOfGuessesRemaining);
         console.log("wordDisplay: " + this.wordDisplay);
-        console.log("isWinner: " + this.isWinner);
+        console.log("isGameOver: " + this.isGameOver);
         console.log("lettersGuessed: " + this.lettersGuessed);
         console.log("========================================================")
     },
@@ -186,7 +269,7 @@ window.onload = function() {
     // This function is run whenever the user presses a key.
     document.onkeyup = function(event) {
 
-        if (game.numberOfGuessesRemaining === 0 || game.isWinner) {
+        if (game.numberOfGuessesRemaining === 0 || game.isGameOver) {
             game.startNewGame();
         }
 
